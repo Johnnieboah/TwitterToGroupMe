dc315f4bfe15455a4d050ce189ca7dd1b1b3d629
Johnnieboah <johnnieestep21@gmail.com>
2016-01-21 03:31:29 -0600

    Update Variables.php

--- Variables.php
+++ Variables.php
@@ -2,15 +2,15 @@
 
 // GroupMe Vars:
 // Your access token can be found here: https://dev.groupme.com/bots
-$access_token                ='youraccesstoken';
+$access_token                ='OaaPHy2YCOqu4LcsJmoDYH8fovf6SrSrFiJmFCXP';
 
 // Your group id can be found by running this curl script: curl https://api.groupme.com/v3/groups?token=youraccesstoken
-$group_id                    ='yourgroupid';
+$group_id                    ='9829189';
 
-$bot_id                      ='yourbotid';
+$bot_id                      ='1a465b29881b91ae8a5fb7b375';
 
 // These all come from your Twitter app:
-$oauth_access_token         = 'yourtwitteroauthaccesstoken';
-$oauth_access_token_secret  = 'yoursecretoauthtwitteraccesstoken';
-$consumer_key               = 'yourconsumerkey';
-$consumer_secret            = 'yoursecretconsumerkey';
+$oauth_access_token         = '3693404057-ls5Kdz1Ms7RmKzkTheMXiK1OTY981XSXkAHEIoR';
+$oauth_access_token_secret  = 'cQIDODvcp0VPXSo3MnGwd9SnfSJRJsSH3GpCosrSLLv2x';
+$consumer_key               = 'Mt8OE70gBoQ47sbegUQ13hFcD';
+$consumer_secret            = '8PvqShaA2D2frlR2foiYAIJcTG25yzTJ0SIkX9C83NOTQUUaOL';
